8bc985a1db966e0af37eeaeb5bff355272145b19
moomoolo <772419236@qq.com>
2022-05-03 15:43:59 +0800

    增加代码高亮

--- src/component/Blog/index.tsx
+++ src/component/Blog/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import style from "./Blog.module.scss";
 import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+import { useEffect } from "react";
 
 export interface BlogProps {
   contentHTML: string
 }
 
 export default function Blog({ contentHTML }: BlogProps) {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [])
   return (
     <div className={style.container}>
       <div className={style.content} dangerouslySetInnerHTML={{__html: contentHTML}}></div>
