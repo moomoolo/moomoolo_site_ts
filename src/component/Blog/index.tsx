55be894c70814749588553c2418b24609a974916
moomoolo <772419236@qq.com>
2022-05-05 12:00:44 +0800

    增加数学公式展示功能

--- src/component/Blog/index.tsx
+++ src/component/Blog/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import style from "./Blog.module.scss";
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
 import { useEffect } from "react";
-
 export interface BlogProps {
-  contentHTML: string
+  contentHTML: string;
 }
 
 // 博客组件，接收html，展示内容并高亮代码
@@ -13,10 +12,17 @@ export default function Blog({ contentHTML }: BlogProps) {
   useEffect(() => {
     // 代码高亮
     Prism.highlightAll();
-  }, [])
+    // 使用MathJax
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+    document.head.appendChild(script);
+  }, []);
   return (
     <div className={style.container}>
-      <div className={style.content} dangerouslySetInnerHTML={{__html: contentHTML}}></div>
+      <div
+        className={style.content}
+        dangerouslySetInnerHTML={{ __html: contentHTML }}
+      ></div>
     </div>
-  )
+  );
 }
